627310caea1cbe584b8f60fbc43b6ccc6469bfb5
Natali <shatabalova2011@gmail.com>
2022-10-19 07:45:27 +0700

    final work

---
+++ public/homePage.js
@@ -0,0 +1,99 @@
+'use strict'
+
+
+
+// выход из личного кабинета
+const logoutButton = new LogoutButton();
+
+logoutButton.action = () => ApiConnector.logout(response => {
+    if (response.success) {
+        location.reload();
+    }
+});
+
+// получение инф о пользователе
+ApiConnector.current(response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+    }
+});
+
+// получение курсов валют
+const ratesBoard = new RatesBoard();
+
+function getRatesBoard() {
+    ApiConnector.getStocks(response => {
+        if (response.success) {
+            ratesBoard.clearTable();
+            ratesBoard.fillTable(response.data);
+        } 
+    });
+}
+
+getRatesBoard();
+setInterval(getRatesBoard, 60000);
+
+//Операции с деньгами
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) => ApiConnector.addMoney(data, response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, "Успешно");
+    } else {
+        moneyManager.setMessage(response.success, response.error);
+    }
+});
+
+moneyManager.conversionMoneyCallback = (data) => ApiConnector.convertMoney(data, response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, "Успешно");
+    } else {
+        moneyManager.setMessage(response.success, response.error);
+    }
+})
+
+moneyManager.sendMoneyCallback = (data) => ApiConnector.transferMoney(data, response => {
+    if (response.success) {
+        ProfileWidget.showProfile(response.data);
+        moneyManager.setMessage(response.success, "Перевод выполнен");
+    } else {
+        moneyManager.setMessage(response.success, response.error);
+    }
+});
+
+//работа с избранным
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites(response =>{
+    if (response.success){
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data)
+    }
+});
+
+favoritesWidget.addUserCallback = (data) => ApiConnector.addUserToFavorites(data, response => {
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+        favoritesWidget.setMessage(response.success, "Пользователь добавлен");
+    } else {
+        favoritesWidget.setMessage(response.success, response.error);
+    }
+});
+
+favoritesWidget.removeUserCallback = (data) => ApiConnector.removeUserFromFavorites(data, response => {
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+        favoritesWidget.setMessage(response.success, "Пользователь удален");
+    } else {
+        favoritesWidget.setMessage(response.success, response.error);
+    }
+});
---
+++ public/loginPage.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = (data) => {
+    ApiConnector.login(data, (response) => {
+        if (response.success) {
+            this.location.reload();
+        } else {
+            this.setLoginErrorMessage(response.error);
+        }
+    });
+};
+
+userForm.registerFormCallback = (data) => {
+    ApiConnector.register(data, (response) => {
+        if (response.success) {
+            this.location.reload();
+        } else {
+            this.setRegisterErrorMessage(response.error);
+        }
+    });
+};
